2978cb8b6d0f0829ddef2a5d30e83c8822b55029
bestrookie <491051587@qq.com>
2021-08-03 21:24:22 +0800

    Initial commit

---
+++ src/main/java/com/bestrookie/SpringBootCodeLearnApplication.java
@@ -0,0 +1,16 @@
+package com.bestrookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootCodeLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCodeLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestrookie/springbootcodelearn/SpringBootCodeLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestrookie.springbootcodelearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCodeLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
